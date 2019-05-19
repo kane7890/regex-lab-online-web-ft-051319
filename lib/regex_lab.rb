c8d0511ae908115b48d33a1813c021968fd85a3a
Kane Koo <kanekoo@gmail.com>
2019-05-19 17:29:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,19 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  word.match(/^[AEIOUaeiou]/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  # textarray=[]
+  # text.split.each do |word|
+  #   binding.pry
+  #   if word.match(/^un+\w+ing$/) != nil
+  #       textarray << word
+  #     end
+  #   end
+  #   textarray
+  text.split.select {|word| word.match(/^un+\w+ing$/)}
 end
 
 def words_five_letters_long(text)
