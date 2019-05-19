9318507cc80e92751daab0a263339ad489bea7b7
Kane Koo <kanekoo@gmail.com>
2019-05-19 17:35:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,7 +21,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.split.select {|word| word.match(/^[A-Z]+\w+\p)}
+  text.split.select {|word| word.match(/^[A-Z]+\w+\p/)}
 end
 
 def valid_phone_number?(phone)
